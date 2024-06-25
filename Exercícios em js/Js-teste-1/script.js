05189e40df280c4eedbb582f201cb8973a7f17d5
Gabriel Souza <122999415+nameboytxt@users.noreply.github.com>
2024-06-25 04:35:39 -0300

    Add files via upload

---
+++ Exercícios em js/Js-teste-1/script.js
@@ -0,0 +1,23 @@
+/*
+var numero = 9;
+
+if(numero > 11){
+    console.log('Este numero é maior que 11');
+}
+else if(numero === 11) {  // Usar "===" para comparação de igualdade estrita
+    console.log('Este numero é igual a 11');
+}
+else {  // O "else" não precisa de condição
+    console.log('Este numero é menor que 11');
+}
+*/
+ 
+
+/*
+var nome = "Gabriel";
+var idade = 19;
+
+if(idade === 19 && nome === "Gabriel"){
+    console.log("Verdade Gabriel tem 19 anos");
+}
+*/
