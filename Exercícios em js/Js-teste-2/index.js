9494a55a539a006ccae6b3fe984088e09a7aee5a
Gabriel Souza <122999415+nameboytxt@users.noreply.github.com>
2024-06-25 04:43:52 -0300

    Add files via upload

---
+++ Exercícios em js/Js-teste-2/index.js
@@ -0,0 +1,6 @@
+        var anoNascimento = prompt ("Por favor informar o ano de nascimento!");
+        var anoAtual = prompt ("Por favor informar o ano atual!");
+
+        var idadeFinal = anoAtual - anoNascimento;
+
+        alert('A idade final é:'+ idadeFinal);
